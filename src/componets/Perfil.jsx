a42e284756880625adc2a7dd26c4fa211191abf9
Kevivin Rojas <Kevinrojaszhuma@icloud.com>
2025-03-14 08:14:40 +0100

    Añadiendo Perfil view, Registrate view, Historico view...

--- src/componets/Perfil.jsx
+++ src/componets/Perfil.jsx
@@ -1,16 +1,31 @@
 import { use, useState } from "react"
+import { useNavigate } from "react-router";
 import axios from 'axios';
 
-
-
 export default function Perfil(){
+    const navigate = useNavigate()
+
+    async function handleHistorico(){
+        navigate('/historico')
+
+    } 
 
 
 
     return (
-        <section className="containerPerfil">
-            <h1>perfil</h1>
-        </section>
+        <>
+            <section className="containerPerfil">
+                <h1>Bono 10 baños</h1>
+                <figure className="perfilSlaider">
+                    <img src="../public/qr.svg" alt="qrBono" />
+                    <img src="../public/qr.svg" alt="qrbono" />
+
+                </figure>
+                <p>Te quedan 5 entradas</p>
+                <button onClick={handleHistorico}>Ver historico</button>
+
+            </section>
+        </>
 
 )
 
